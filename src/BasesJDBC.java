0fd8ecef82d2fec52deebe7e39776e071c3903ed
mathe <mathe@172.16.138.31>
2024-05-24 11:37:35 +0200

    aaaa

--- src/BasesJDBC.java
+++ src/BasesJDBC.java
@@ -1,82 +1,88 @@
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.Statement;
-//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
+
 public class BasesJDBC {
 	public static void main(String[] args) throws Exception {
 		Class.forName( "org.hsqldb.jdbcDriver" );
+
 		String url = "jdbc:hsqldb:file:database"+File.separator+"basic;shutdown=true";
 		String login = "sa";
 		String password = "";
-		
 		try (Connection connection = DriverManager.getConnection( url, login, password )){
-			String requete = "DROP TABLE client IF EXISTS;"+"DROP TABLE entrepot IF EXISTS;"
-							  + "DROP TABLE site IF EXISTS;" + "DROP TABLE route IF EXISTS;" +"DROP TABLE demande IF EXISTS;";
+
+			String requete = "DROP TABLE CLIENTS IF EXISTS;";
 			try ( Statement statement = connection.createStatement() ) {
-			statement.executeUpdate( requete );
-			}	
-			requete = "CREATE TABLE entrepot ("
-					+"IDentrepot int,"
-					+"Stock disponible int,"
-					+"IDlocalisation int,";
-					try ( Statement statement = connection.createStatement() ) {
-					statement.executeUpdate( requete );
-					}
-					
-			requete = "CREATE TABLE client ("
-							+"nom varchat(20),"
-							+"IDgeo int,"
-							+"Email varchar(50),";
-							 
-							try ( Statement statement = connection.createStatement() ) {
-							statement.executeUpdate( requete );
-							}
-							
-			requete = "CREATE TABLE  site("
-							+"X int,"
-							+"Y int,"
-							+"IDsite int,";
-							try ( Statement statement = connection.createStatement() ) {
-							statement.executeUpdate( requete );
-							}
-							
-			requete = "CREATE TABLE route ("
-							+"IDorigine int,"
-							+"IDarrive int,";
-							try ( Statement statement = connection.createStatement() ) {
-							statement.executeUpdate( requete );
-							}
-							
-			requete = "CREATE TABLE demande ("
-							+"IDclient,"
-							+"Quantite demande int,";
-							try ( Statement statement = connection.createStatement() ) {
-							statement.executeUpdate( requete );
-							}
-			
-			
-				
-			/*requete = "INSERT INTO citation (annee, citation, auteur) VALUES"
-					+"(1947, 'La simplicite est la reussite absolue', 'Frederic Chopin'),"
-					+"(1979, 'Le bonheur, tu sauras que c''est la simplicite', 'Jacques Brillant'),"
-					+"(1986, 'La simplicite est la cle de la reussite','Andre Rochette')";
-					try ( Statement statement = connection.createStatement() ) {
-					statement.executeUpdate( requete );
-					}*/
-		
-			requete = "SELECT * FROM entrepot";
-					try ( Statement statement = connection.createStatement() ) {
-					try (ResultSet resultSet = statement.executeQuery( requete ) ) {
-					while( resultSet.next() ) {
-					int annee = resultSet.getInt( "annee" );
-					String auteur = resultSet.getString( "auteur" );
-					String citation = resultSet.getString( "citation" );
-					System.out.println(String.format("%-5d %-20s %s", annee,auteur,citation));
-					}
-					}
-					}
+				statement.executeUpdate( requete );
+			}
+			requete = "DROP TABLE SITES IF EXISTS;";
+			try ( Statement statement = connection.createStatement() ) {
+				statement.executeUpdate( requete );
+			}
+			requete = "DROP TABLE ROUTES IF EXISTS;";
+			try ( Statement statement = connection.createStatement() ) {
+				statement.executeUpdate( requete );
+			}
+			requete = "DROP TABLE ENTREPOTS IF EXISTS;";
+			try ( Statement statement = connection.createStatement() ) {
+				statement.executeUpdate( requete );
+			}
+			requete = "DROP TABLE COMMANDE IF EXISTS;";
+			try ( Statement statement = connection.createStatement() ) {
+				statement.executeUpdate( requete );
+			}
+			requete = "CREATE TABLE CLIENTS ("
+					+"id site int,"
+					+"mail varchar(50),"
+					+"nom varchar(20),"
+					+"PRIMARY KEY(mail)"
+					+"FOREIGN KEY(id site) reference SITES(id site))";
+
+			try ( Statement statement = connection.createStatement() ) {
+				statement.executeUpdate( requete );
+			}
+
+			requete = "CREATE TABLE SITES ("
+					+"id site int,"
+					+"x int,"
+					+"y int,"
+					+ "PRIMARY KEY(id site))";
+			try ( Statement statement = connection.createStatement() ) {
+				statement.executeUpdate( requete );
+			}
+
+			requete = "CREATE TABLE ROUTES ("
+					+"origine int,"
+					+"destination int,"
+					+"PRIMARY KEY(origine, destination))";
+			try ( Statement statement = connection.createStatement() ) {
+				statement.executeUpdate( requete );
+			}
+			requete = "CREATE TABLE ENTREPOTS ("
+					+" id entrepot int,"
+					+"id site int,"
+					+ "cout fixe int,"
+					+ " stock int,"
+					+ "PRIMARY KEY (id entrepot)"
+					+ " FOREIGN KEY (id site) reference SITES(id site))";
+			try ( Statement statement = connection.createStatement() ) {
+				statement.executeUpdate( requete );
+			}
+			requete = "CREATE TABLE COMMANDE ("
+					+" date int,"
+					+"mail varchar(50),"
+					+ "quantite demande int,"
+					+ "entrepots dispo int,"
+					+"id entrepot int"
+					+ "PRIMARY KEY (date)"
+					+ "FOREIGN KEY (mail) reference CLIENTS(mail)"
+					+ "FOREIGN KEY (id entrepot) reference ENTREPOTS(id entrepot))";
+
+			try ( Statement statement = connection.createStatement() ) {
+				statement.executeUpdate( requete );
+			}
+
 		}
-		}
+	}
 }
