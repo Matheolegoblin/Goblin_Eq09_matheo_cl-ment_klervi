78149b922b9340f3e6fe992d74282b720a3b0ab7
mathe <mathe@172.16.138.31>
2024-05-27 14:11:57 +0200

    json

--- src/ReadCSVData.java
+++ src/ReadCSVData.java
@@ -1,10 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader; 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
-
-
-import com.opencsv.bean.CsvToBeanBuilder;
 
 /*public class ReadCSVData {
 
@@ -24,9 +20,6 @@ import com.opencsv.bean.CsvToBeanBuilder;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
 
 public class ReadCSVData {
     public static void main(String[] args) {
