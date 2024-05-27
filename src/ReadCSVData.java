11b376f240546f34ad6c91ad14cff880b289a41f
clementcardona <clementcardona@localhost>
2024-05-27 14:52:37 +0200

    pas ouf

--- src/ReadCSVData.java
+++ src/ReadCSVData.java
@@ -1,31 +1,24 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader; 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 
 import com.opencsv.bean.CsvToBeanBuilder;
 
 /*public class ReadCSVData {
-
 	private static final String CSV_FILE_CUSTOM_SEPARATOR 
     = "Jeux_de_donnees"+File.separator+"petit"+File.separator+"init-clients-30-10-Carre.csv"; 
-	
-	public static void main(String[] args) 
-	{ 
-		List<Client> beans = new CsvToBeanBuilder(new FileReader(CSV_FILE_CUSTOM_SEPARATOR ))
-				.withType(Client.class).withSeparator(';').build().parse();
-		System.out.println(beans);
-
-
+@@ -20,4 +16,25 @@ public static void main(String[] args)
 	}
-
 }*/
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.FileReader;	
+import java.io.IOException;	
 import java.util.List;
 
 public class ReadCSVData {
