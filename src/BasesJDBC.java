4559924eb5c6b86c5ed860ef39ca4e1b28ad27df
clementcardona <clementcardona@localhost>
2024-06-07 18:05:09 +0200

    modif

--- src/BasesJDBC.java
+++ src/BasesJDBC.java
@@ -5,23 +5,44 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class BasesJDBC {
-	public static void main(String[] args) throws Exception {
-		Class.forName( "org.hsqldb.jdbcDriver" ); 
 
-		String url = "jdbc:hsqldb:file:database"+File.separator+"basic;shutdown=true";
-		String login = "sa";
-		String password = "";
+	private static String url = "jdbc:hsqldb:file:database"+File.separator+"basic;shutdown=true";
+	private static String login = "sa";
+	private static String password = "";
+
+	public static void MaJBdD() {
+
+		try (Connection connection = DriverManager.getConnection( url, login, password )){
+			String requete = "UPDATE ENTREPOTS SET stock = '" +;
+			try ( Statement statement = connection.createStatement() ) {
+				try (ResultSet resultSet = statement.executeQuery( requete ) ) {	
+				}
+
+			}
+
+			requete = "SELECT stock FROM ENTREPOTS";
+			try ( Statement statement = connection.createStatement() ) {
+				try (ResultSet resultSet = statement.executeQuery( requete ) ) {
+
+					System.out.println("Stock mise a jour !");
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Class.forName( "org.hsqldb.jdbcDriver" );
 		try (Connection connection = DriverManager.getConnection( url, login, password )){
 
 			String requete = "DROP TABLE COMMANDE IF EXISTS;" 
-			+ "DROP TABLE ENTREPOTS IF EXISTS;"
-			+"DROP TABLE ROUTES IF EXISTS;"
-			+"DROP TABLE CLIENTS IF EXISTS;"
-			+"DROP TABLE SITE IF EXISTS;";
+					+ "DROP TABLE ENTREPOTS IF EXISTS;"
+					+"DROP TABLE ROUTES IF EXISTS;"
+					+"DROP TABLE CLIENTS IF EXISTS;"
+					+"DROP TABLE SITE IF EXISTS;";
 			try ( Statement statement = connection.createStatement() ) {
 				statement.executeUpdate( requete );
 			}
-			
+
 			requete = "CREATE TABLE SITE ("
 					+"id_site int,"
 					+"x int,"
@@ -31,11 +52,11 @@ public class BasesJDBC {
 				statement.executeUpdate( requete );
 			}
 			requete = "CREATE TABLE CLIENTS ("
-			        + "id_site int,"
-			        + "mail varchar(50),"
-			        + "nom varchar(20),"
-			        + "PRIMARY KEY(mail),"
-			        + "FOREIGN KEY(id_site) REFERENCES SITE(id_site))";
+					+ "id_site int,"
+					+ "mail varchar(50),"
+					+ "nom varchar(20),"
+					+ "PRIMARY KEY(mail),"
+					+ "FOREIGN KEY(id_site) REFERENCES SITE(id_site))";
 
 			try ( Statement statement = connection.createStatement() ) {
 				statement.executeUpdate( requete );
@@ -49,38 +70,37 @@ public class BasesJDBC {
 				statement.executeUpdate( requete );
 			}
 			requete = "CREATE TABLE ENTREPOTS ("
-			        + "id_entrepot int,"
-			        + "id_site int,"
-			        + "cout_fixe int,"
-			        + "stock int,"
-			        + "PRIMARY KEY(id_entrepot),"
-			        + "FOREIGN KEY(id_site) REFERENCES SITE(id_site))";
+					+ "id_entrepot int,"
+					+ "id_site int,"
+					+ "cout_fixe int,"
+					+ "stock int,"
+					+ "PRIMARY KEY(id_entrepot),"
+					+ "FOREIGN KEY(id_site) REFERENCES SITE(id_site))";
 			try ( Statement statement = connection.createStatement() ) {
 				statement.executeUpdate( requete );
 			}
 			requete = "CREATE TABLE COMMANDE ("
-			        + "date int,"
-			        + "mail varchar(50),"
-			        + "quantite_demande int,"
-			        + "entrepots_dispo int,"
-			        + "id_entrepot int,"
-			        + "PRIMARY KEY (date),"
-			        + "FOREIGN KEY (mail) REFERENCES CLIENTS(mail),"
-			        + "FOREIGN KEY (id_entrepot) REFERENCES ENTREPOTS(id_entrepot))";
+					+ "date int,"
+					+ "mail varchar(50),"
+					+ "quantite_demande int,"
+					+ "entrepots_dispo int,"
+					+ "id_entrepot int,"
+					+ "PRIMARY KEY (date),"
+					+ "FOREIGN KEY (mail) REFERENCES CLIENTS(mail),"
+					+ "FOREIGN KEY (id_entrepot) REFERENCES ENTREPOTS(id_entrepot))";
 
 			try ( Statement statement = connection.createStatement() ) {
 				statement.executeUpdate( requete );
 			}
-			
-			
+
+
 			requete = "SELECT stock FROM ENTREPOTS";
 			try ( Statement statement = connection.createStatement() ) {
 				try (ResultSet resultSet = statement.executeQuery( requete ) ) {
-				
-			System.out.println("Table créer avec succés !");
+
+					System.out.println("Table créer avec succés !");
+				}
+			}
 		}
 	}
-
-}
-}
 }
